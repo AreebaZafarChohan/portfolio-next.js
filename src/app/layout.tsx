a2cd8035055927d1fe41ce50292185ed5759c6cc
AreebaZafarChohan <areebazafar715@gmail.com>
2024-09-29 19:39:52 +0500

    some css changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      < Meteors number={20}/>
       <Navbar />
       {children}
       <Footer />

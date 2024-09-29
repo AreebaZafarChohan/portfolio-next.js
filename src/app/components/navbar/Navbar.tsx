8ad7bd7c7cff9860f0a0a1f2cdb713018b703f58
AreebaZafarChohan <areebazafar715@gmail.com>
2024-09-29 19:34:02 +0500

    some css changes

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     const [isNavbarOpen, setIsNavbarOpen] = useState(false); 
   return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
-      <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
+      <div className='flex flex-wrap items-center justify-between px-4 py-2'>
       <Link href={"./"} className=' bg-gradient-to-r from-purple-600 via-pink-600 to-blue-500 rounded-full p-1'>
       <Image 
       src="/images/logo.jpg"
